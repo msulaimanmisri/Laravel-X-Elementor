60c5ed58e900b15650d27a5438da5784f16b561a
Sulaiman Desktop <msulaimanmisri@gmail.com>
2022-10-10 22:54:59 +0800

    Create master layout and DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,18 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard.index');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
